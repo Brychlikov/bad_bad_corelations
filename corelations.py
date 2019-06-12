bfdc969485c9306d627d6b814df872590d3223cd
Brychlikov <brychlikow@gmail.com>
2019-06-13 00:10:30 +0200

    Last push

--- corelations.py
+++ corelations.py
@@ -1,4 +1,5 @@
 from fetcher import DataCollection, DataEntry
+import argparse
 from scipy.stats.stats import pearsonr as scipyp
 from copy import copy
 from random import shuffle
@@ -12,35 +13,46 @@ def false_correlation(e1, e2):
         p_short = pearsonr(e1.data[2:], e2.data[2:])
     except ValueError:
         return False
-    return abs(p_short) > 0.925 and abs(p_long) < 0.35
+    return abs(p_short) > 0.95 and abs(p_long) < 0.2
 
-db = DataCollection.read('data3')
 
-data = db.return_range(2000, 2010)
-data = [e for e in data if 'ogółem' not in e.name and 'sektor' not in e.name]
-shuffle(data)
-print(len(data))
-input()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Finds false correlations in data directory and saves them in a file")
+    parser.add_argument('-i', help="Directory containing data")
+    parser.add_argument('-r', '--range', help="Range for corelations")
 
-with open('pairs.txt', 'w') as file:
-    result = []
+    args = parser.parse_args()
 
-    try:
-        for e1 in data:
-            temp_data = copy(data)
-            shuffle(temp_data)
-            for e2 in temp_data:
-                if e1 is e2 or e1.region == e2.region:
-                    continue
-                try:
-                    p1 = pearsonr(e1.data, e2.data)
-                    p2 = pearsonr(e1.data[2:], e2.data[2:])
-                except ValueError:
-                    continue
-                if false_correlation(e1, e2):
-                    file.write(f"data2/{e1.res_id}/{e1.region}.json, data2/{e2.res_id}/{e2.region}.json, {p1}, {p2}\n")
-                    file.flush()
-                    # print(f"Correlation between \n{e1.name} -- {e1.topic} \n {e2.name} -- {e2.topic} \n is {p}")
-                    # print()
-    except KeyboardInterrupt:
-        pass
+    db = DataCollection.read(args.i)
+
+    start, end = map(int, args.range.split('-'))
+    data = db.return_range(start, end)
+    data = [e for e in data if 'ogółem' not in e.name and 'sektor' not in e.name]
+    shuffle(data)
+    print(len(data))
+
+    with open('pairs.txt', 'w') as file:
+        result = []
+
+        file.write(args.range + "\n")
+
+        try:
+            for e1 in data:
+                temp_data = copy(data)
+                shuffle(temp_data)
+                for e2 in temp_data:
+                    if e1 is e2 or e1.region == e2.region:
+                        continue
+                    try:
+                        p1 = pearsonr(e1.data, e2.data)
+                        p2 = pearsonr(e1.data[2:], e2.data[2:])
+                    except ValueError:
+                        continue
+                    if false_correlation(e1, e2):
+                        file.write(f"{args.i}/{e1.res_id}/{e1.region}.json, {args.i}/{e2.res_id}/{e2.region}.json, {p1}, {p2}\n")
+                        file.flush()
+                        break
+                        # print(f"Correlation between \n{e1.name} -- {e1.topic} \n {e2.name} -- {e2.topic} \n is {p}")
+                        # print()
+        except KeyboardInterrupt:
+            pass
--- plotting.py
+++ plotting.py
@@ -14,15 +14,15 @@ def simple_plot(e1: DataEntry, e2: DataEntry, text="", savename=None):
     mag_order1 = round(log10(e1.data[0]))
     mag_order2 = round(log10(e2.data[0]))
 
-    ax1.plot(x, [i / 10 ** mag_order1 for i in e1.data], c="r", label=e1.name[:e1.name.find('-')])
+    ax1.plot(x, e1.data, c="r", label=e1.name[:e1.name.find('-')])
     ax1.ticklabel_format(useOffset=False)
-    ax2.plot(x, [i / 10 ** mag_order2 for i in e2.data], c="b", label=e2.name[:e2.name.find('-')])
+    ax2.plot(x, e2.data, c="b", label=e2.name[:e2.name.find('-')])
     ax2.ticklabel_format(useOffset=False)
     ax1.legend(loc="upper right")
     ax2.legend(loc="upper left")
     ax1.set_xlabel('Rok')
-    ax1.set_ylabel(e1.unit + f" 10^{mag_order1}", c="r")
-    ax2.set_ylabel(e2.unit + f" 10^{mag_order2}", c="b")
+    ax1.set_ylabel(e1.unit, c="r")
+    ax2.set_ylabel(e2.unit, c="b")
     plt.title(f"Correlation between: {e1.region} - {e1.name} {e1.topic.split('-')[-1]} \n and \n {e2.region} - {e2.name} {e2.topic.split('-')[-1]}", wrap=True)
 
     if savename:
@@ -40,25 +40,18 @@ def simple_plot(e1: DataEntry, e2: DataEntry, text="", savename=None):
 if __name__ == "__main__":
 
     RESULT_PATH = 'results'
+    REAL_RESULT_PATH = 'real_results'
 
-    
-    i = 0
-    for line in open('pairs.txt'):
-        data = line.split(',')
-        e1 = DataEntry.from_json(open(data[0].strip())).get_range(2002, 2010)
-        e2 = DataEntry.from_json(open(data[1].strip())).get_range(2002, 2010)
-        simple_plot(e1, e2, f"p1={data[2]}  p2={data[3]}", os.path.join(RESULT_PATH, f'{i}.png'))
-        i += 1
-
-    # pair = (3146, 2131)
-    # e1 = DataEntry.from_json(open(f'data2/{pair[0]}/POLSKA.json')).get_range(2000, 2010)
-    # e2 = DataEntry.from_json(open(f'data2/{pair[1]}/POLSKA.json')).get_range(2000, 2010)
-    # db = DataCollection.read('data2')
-    # res = db.return_range(2000, 2010)
-    #
-    # print(len(res))
-    # res = [e for e in res if len(e.data) == e.end_year - e.start_year]
-    # print(len(res))
-    # shuffle(res)
-    # e1 = res[0]
-    # e2 = res[1]
+    with open('pairs.txt') as file:
+        start, end = map(int, file.readline().split('-'))
+        
+        i = 0
+        for line in file:
+            data = line.split(',')
+            e1 = DataEntry.from_json(open(data[0].strip())).get_range(start + 2, end)
+            e2 = DataEntry.from_json(open(data[1].strip())).get_range(start + 2, end)
+            simple_plot(e1, e2, f"p1={data[2]}  p2={data[3]}", os.path.join(RESULT_PATH, f'{i}.png'))
+            e3 = DataEntry.from_json(open(data[0].strip())).get_range(start, end)
+            e4 = DataEntry.from_json(open(data[1].strip())).get_range(start, end)
+            simple_plot(e3, e4, f"p1={data[2]}  p2={data[3]}", os.path.join(REAL_RESULT_PATH, f'{i}-real.png'))
+            i += 1
